70322d7587f5c6964f72aeb1a9df3a58568046c3
pradeep saini <pradeepsaini384@gmail.com>
2023-08-22 19:58:33 +0530

    first commit

---
+++ app.py
@@ -0,0 +1,71 @@
+import openai
+from linkedin_scraper import Person, actions
+from selenium import webdriver
+from flask import Flask,request , render_template,redirect
+import os 
+app = Flask(__name__)
+
+
+memory =  []
+def add_memory(url,person):
+    # d = "{'persson':'{url}','details':'{person}'}".format(persson=url,details= person)
+    # print(dict(d))
+    dic = {
+        "name": url ,
+        "details": person
+    }
+    
+    memory.append(dic)
+    print(memory)
+def check_memory(name):
+    for i in memory:
+        if name in i['name']:
+            return i['details']
+    else:
+        return None
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+@app.route('/run',methods=['GET','POST']) 
+def run():
+    url = request.form.get('url')
+    already_Store = check_memory(url)
+    if already_Store is None:
+        email = os.environ.get('email')
+        password = os.environ.get('password')
+        driver = webdriver.Chrome()
+        actions.login(driver, email, password) # if email and password isnt given, it'll prompt in terminal
+        person = Person(f"{url}", driver=driver)
+        add_memory(url,person)
+        output = call_ai(already_Store)
+        return output
+    else :
+        output = call_ai(already_Store)
+        return output
+def call_ai(person):
+    api_key = "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"
+
+# Initialize the OpenAI API client
+    openai.api_key = api_key
+    prompt = f"""You Are A Best Ai Tool who can generate a short paragraph for linkedin About section .
+             You have the ability to understand the parameter according to a list or python generated output . 
+             this output have very short details on about his name ,college , Experience and his skills .
+             so being a smart reader you have to understand all parameters and generate a summary of  on his given details and make sure the output format  is like a person expressing about himself or give a interview and interviewer asking about yourself.
+             details : {person}
+    """
+
+    # Generate a response using the OpenAI GPT-3.5 Turbo model
+    response = openai.Completion.create(
+        engine="text-davinci-003",  # GPT-3.5 Turbo engine
+        prompt=prompt,
+        max_tokens=500,  # Adjust the length of the generated response
+        stop=None,  # You can specify a stop word to end the response if needed
+    )
+
+    # Extract and print the generated answer
+    answer = response.choices[0].text.strip()
+    return(f"Generated Answer: {answer}")
+
+if __name__ == "__main__":
+    app.run(debug=True)
