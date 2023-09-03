af9250f78447da5bc60a14085a44c571e14f77ab
pradeep saini <pradeepsaini384@gmail.com>
2023-09-03 22:30:45 +0530

    Some Optimization

--- app.py
+++ app.py
@@ -19,6 +19,20 @@ def check_user_in_json(url):
     for i in data:
         if url in i['name']:
             return i['details']
+    else:
+        return None
+def user_data_in_json(url):
+    data= load_from_json(file_name)
+    for i in data:
+        if url in i['name']:
+            return i['output']
+        else:
+            return None
+def check_user_len_in_json(url):
+    data= load_from_json(file_name)
+    for i in data:
+        if url in i['name']:
+            return len(i['output'])
         else:
             return None
 def save_new_user_in_json(url,person):
@@ -65,27 +79,32 @@ def output():
         output = call_ai(already_Store,url)
         return output
 def call_ai(person,url):
-    api_key = os.environ.get('OPENAI_API_KEY')
-    # Initialize the OpenAI API client
-    openai.api_key  = api_key
-    prompt = f"""You Are A Best Ai Tool who can generate a short paragraph for linkedin About section .
-             You have the ability to understand the parameter according to a list or python generated output . 
-             this output have very short details on about his name ,college , Experience and his skills .
-             so being a smart reader you have to understand all parameters and generate a summary of  on his given details and make sure the output format  is like a person expressing about himself or give a interview and interviewer asking tell me about yourself.
-             details : {person}
-    """
-    # Generate a response using the OpenAI GPT-3.5 Turbo model
-    response = openai.Completion.create(
-        engine="text-davinci-003",  # GPT-3.5 Turbo engine
-        prompt=prompt,
-        max_tokens=500,  # Adjust the length of the generated response
-        stop=None, 
-        temperature= 0.5 # You can specify a stop word to end the response if needed
-    )
+    user_len= check_user_len_in_json(url)
+    if(user_len)==2:
+        answer = user_data_in_json(url)
+        return redirect(url_for('result',url= url,answer=answer))
+    else:
+        api_key = os.environ.get('OPENAI_API_KEY')
+        # Initialize the OpenAI API client
+        openai.api_key  = api_key
+        prompt = f"""You Are A Best Ai Tool who can generate a short paragraph for linkedin About section .
+                You have the ability to understand the parameter according to a list or python generated output . 
+                this output have very short details on about his name ,college , Experience and his skills .
+                so being a smart reader you have to understand all parameters in details and generate a summary of  on his given details and make sure the output format  is like a person expressing about himself or give a interview and interviewer asking tell me about yourself.
+                details = {person}
+        """
+        # Generate a response using the OpenAI GPT-3.5 Turbo model
+        response = openai.Completion.create(
+            engine="text-davinci-003",  # GPT-3.5 Turbo engine
+            prompt=prompt,
+            max_tokens=500,  # Adjust the length of the generated response
+            stop=None, 
+            temperature= 0.5 # You can specify a stop word to end the response if needed
+        )
 
-    # Extract and print the generated answer
-    answer = response.choices[0].text.strip()
-    return redirect(url_for('result',url= url,answer=answer))
+        # Extract and print the generated answer
+        answer = response.choices[0].text.strip()
+        return redirect(url_for('result',url= url,answer=answer))
 @app.route('/result')
 def result():
     url = request.args.get('url')
